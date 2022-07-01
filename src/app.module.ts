2307c8eb6bebd5b42dd768486e305bb371a95e7c
RLazamh <roger.laza@epn.edu.ec>
2022-07-01 06:34:45 -0500

    add contection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { typeOrmConfig } from './config/typeorm.config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule, 
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
