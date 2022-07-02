bab1f75e52c1096d8ae82989b6d2f1e96680d902
RLazamh <roger.laza@epn.edu.ec>
2022-07-02 14:01:45 -0500

    add conection to database and entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AdministrationModule } from './administration/administration.module';
 
 
 @Module({
@@ -13,6 +14,7 @@ import { typeOrmConfig } from './config/typeorm.config';
     UsersModule, 
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeOrmConfig),
+    AdministrationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
